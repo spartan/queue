263757ab633a5eb37dc4e74fb19e604713bf9fcf
Iulian N <iulian@spartanphp.com>
2023-06-27 00:02:00 +0300

    new: add delay parameter to enqueue function

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+use Enqueue\AmqpTools\RabbitMqDlxDelayStrategy;
 use Spartan\Queue\Definition\TaskInterface;
 use Spartan\Queue\Manager;
 
@@ -8,18 +9,24 @@ if (!function_exists('enqueue')) {
     /**
      * @param TaskInterface $task
      * @param string        $queue
+     * @param int           $delay  Delay in seconds (RabbitMQ only)
      *
      * @return Manager
      * @throws \Interop\Queue\Exception
      * @throws \Interop\Queue\Exception\InvalidDestinationException
      * @throws \Interop\Queue\Exception\InvalidMessageException
      */
-    function enqueue(TaskInterface $task, string $queue = Manager::QUEUE_NAME): Manager
+    function enqueue(TaskInterface $task, string $queue = Manager::QUEUE_NAME, int $delay = 0): Manager
     {
         static $manager;
 
         if (!$manager) {
             $manager = Manager::instance(container());
+        }
+
+        if ($delay > 0) {
+            $manager->context()->setDelayStrategy(new RabbitMqDlxDelayStrategy());
+            $manager->producer()->setDeliveryDelay($delay * 1000); // seconds * 1000 = milliseconds
         }
 
         return $manager->withQueue($queue)
