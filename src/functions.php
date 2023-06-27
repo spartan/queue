d1fe771144b67997cd2aad88880023c6328a499f
Iulian N <iulian@spartanphp.com>
2023-06-27 17:18:37 +0300

    new: enqueue_delay function

--- src/functions.php
+++ src/functions.php
@@ -9,14 +9,13 @@ if (!function_exists('enqueue')) {
     /**
      * @param TaskInterface $task
      * @param string        $queue
-     * @param int           $delay  Delay in seconds (RabbitMQ only)
      *
      * @return Manager
      * @throws \Interop\Queue\Exception
      * @throws \Interop\Queue\Exception\InvalidDestinationException
      * @throws \Interop\Queue\Exception\InvalidMessageException
      */
-    function enqueue(TaskInterface $task, string $queue = Manager::QUEUE_NAME, int $delay = 0): Manager
+    function enqueue(TaskInterface $task, string $queue = Manager::QUEUE_NAME): Manager
     {
         static $manager;
 
@@ -24,10 +23,33 @@ if (!function_exists('enqueue')) {
             $manager = Manager::instance(container());
         }
 
-        if ($delay > 0) {
+        return $manager->withQueue($queue)
+                       ->enqueue($task);
+    }
+}
+
+
+if (!function_exists('enqueue_delayed')) {
+
+    /**
+     * @param TaskInterface $task
+     * @param string        $queue
+     *
+     * @return Manager
+     * @throws \Interop\Queue\Exception
+     * @throws \Interop\Queue\Exception\InvalidDestinationException
+     * @throws \Interop\Queue\Exception\InvalidMessageException
+     */
+    function enqueue_delayed(TaskInterface $task, string $queue = Manager::QUEUE_NAME, int $delay = 0): Manager
+    {
+        static $manager;
+
+        if (!$manager) {
+            $manager = Manager::instance(container());
             $manager->context()->setDelayStrategy(new RabbitMqDlxDelayStrategy());
-            $manager->producer()->setDeliveryDelay($delay * 1000); // seconds * 1000 = milliseconds
         }
+
+        $manager->producer()->setDeliveryDelay($delay * 1000); // seconds * 1000 = milliseconds
 
         return $manager->withQueue($queue)
                        ->enqueue($task);
